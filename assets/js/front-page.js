f52a47a1af1adbc23881217ebc384a8cfcd54d71
charliegene14 <charliedenier@yahoo.fr>
2021-05-24 22:43:36 +0200

    front page mousemove

--- assets/js/front-page.js
+++ assets/js/front-page.js
@@ -1,9 +1,6 @@
 if (document.body.classList.contains('home')) {
 
-    function mouseStop() {
-        var background = document.getElementById('clikea-navigation-background');
-        background.style.opacity = "0";
-    }
+    var mouseStopIn = 1;
 
     var navBackgound = document.getElementById('clikea-navigation-background');
     navBackgound.classList.remove('clikea-navigation-background');
@@ -13,9 +10,17 @@ if (document.body.classList.contains('home')) {
     navContainer.classList.add('clikea-navigation-container-home');
 
     document.body.addEventListener('mousemove', e => {
-        clearTimeout();
         var background = document.getElementById('clikea-navigation-background');
         background.style.opacity = "1";
-        setTimeout(mouseStop, 3000);
+        mouseStopIn = 1;
     });
+
+    setInterval(function() {
+        if (mouseStopIn <= 0) {
+            var background = document.getElementById('clikea-navigation-background');
+            background.style.opacity = "0";
+        }
+        mouseStopIn -= 1;
+        
+    }, 1000);
 }
