74b03925ed2e07e8d8ed541d8a131643c835e48e
charliegene14 <charliedenier@yahoo.fr>
2021-05-29 13:16:54 +0200

    shop nav

--- inc/clikea-template-functions.php
+++ inc/clikea-template-functions.php
@@ -1,12 +1,62 @@
 <?php
 
 /**
+ * Shop Navigation start wrapper
+ */
+function clikea_shop_nav_wrap_start() {
+    if (is_woocommerce()):
+    ?>
+        <div id="clikea-shop-navigation-container">
+    <?php
+    endif;
+}
+
+/**
  * Content of shop navigation
  */
 function clikea_shop_nav_content() {
+    if (is_woocommerce()):
+
+    clikea_shop_menu();
+
+    /*clikea_search_wc_widget();
+    clikea_cart_wc_widget();
+    clikea_user_menu();*/
+
     ?>
-        <div id="clikea-shop-navigation-container"></div>
+
     <?php
+    endif;
+}
+
+/**
+ * Shop Navigation end wrapper
+ */
+function clikea_shop_nav_wrap_end() {
+    if (is_woocommerce()):
+    ?>
+        </div>
+    <?php
+    endif;
+}
+
+/**
+ * Main Navigation start wrapper
+ */
+function clikea_main_nav_wrap_start() {
+    ?>
+        <div id="clikea-main-navigation-container" class="clikea-main-navigation-container">
+    <?php
+
+    if(is_front_page()):
+        ?>
+            <div id="clikea-navigation-background" class="clikea-navigation-background-home"></div>
+        <?php
+    else:
+        ?>
+            <div id="clikea-navigation-background" class="clikea-navigation-background"></div>
+        <?php
+    endif;
 }
 
 /**
@@ -14,59 +64,43 @@ function clikea_shop_nav_content() {
  */
 function clikea_main_nav_content() {
     ?>
+
+    <nav class="clikea-navigation">
         <?php wp_nav_menu(array('menu' => 'Navigation gauche')); ?>
         <a class="clikea-navigation-logo" href="/">
             <img src="/wp-content/themes/clikea/assets/img/logo.svg" alt="logo">
         </a>
-        <?php wp_nav_menu(array('menu' => 'Navigation droite')); ?> 
+        <?php wp_nav_menu(array('menu' => 'Navigation droite')); ?>
+    </nav>
 
     <?php
 }
 
 /**
- * Start Normal Page navigation wrapper
+ * Main Navigation end wrapper
+ */
+function clikea_main_nav_wrap_end(){
+    ?>
+            </div>
+        </div>
+    <?php
+}
+
+/**
+ * Header Global navigation start wrapper
  */
 function clikea_nav_wrap_start(){
     if(is_front_page()):
-        ?>
-            <header id="clikea-front-nav-container">
-                <div id="clikea-navigation-container" class="clikea-navigation-container">
-                    <div id="clikea-navigation-background" class="clikea-navigation-background-home"></div>
-                    <nav class="clikea-navigation">
-        <?php
-
+        ?> <header id="clikea-front-nav-container"> <?php
     elseif (is_woocommerce()):
-        ?>
-            <header id="clikea-wc-nav-container">
-                <div id="clikea-navigation-container" class="clikea-navigation-container">
-                    <div id="clikea-navigation-background" class="clikea-navigation-background"></div>
-                    <nav class="clikea-navigation">
-
-
-        <?php
+        ?> <header id="clikea-wc-nav-container"> <?php
     else:
-        ?>
-            <header id="clikea-normal-nav-container">
-                <div id="clikea-navigation-container" class="clikea-navigation-container">
-                    <div id="clikea-navigation-background" class="clikea-navigation-background"></div>
-                    <nav class="clikea-navigation">
-        <?php
+        ?> <header id="clikea-normal-nav-container"> <?php
     endif;
 }
 
 /**
- * Ending main navigation wrapper
- */
-function clikea_main_nav_wrap_end(){
-    ?>
-                    </nav>
-                </div>
-            </div>
-    <?php
-}
-
-/**
- * Ending navigation wrapper
+ * Header Global navigation end wrapper
  */
 function clikea_nav_wrap_end(){
     ?>
@@ -75,15 +109,57 @@ function clikea_nav_wrap_end(){
 }
 
 /**
- * Cart Toggle
- * Appears on cart image click.*/
-function clikea_cart_navigation() {
+ * Search Form Input widget
+ * Called in
+ * #clikea-widget-search-container
+ * .clikea-widget-search-container
+ */
+function clikea_search_wc_widget(){
     ?>
+    <div id="clikea-widget-search-container" class="clikea-widget-search-container">
+        <?php the_widget('WC_Widget_Product_Search', 'title=') ?>
+    </div>
+    <?php
+}
 
-    <nav class="clikea-cart-navigation">
+/**
+ * Cart Widget called in
+ * #clikea-widget-cart-container
+ * .clikea-widget-cart-container
+ */
+function clikea_cart_wc_widget(){
+    ?>
+    <div id="clikea-widget-cart-container" class="clikea-widget-cart-container">
+        <?php the_widget('WC_Widget_Cart', 'title=') ?>
+    </div>
+    <?php
+}
+
+/**
+ * Archive Products menu
+ * Displays in CSS: 
+ * #clikea-shop-menu-container
+ * .clikea-shop-menu-container
+ */
+function clikea_shop_menu(){
+    ?>
+    <nav id="clikea-shop-menu-container" class="clikea-shop-menu-container">
+        <?php wp_nav_menu(array('menu' => 'Boutique')); ?>
+    </nav>
+    <?php
+}
+
+/**
+ * Cart Shopping Menu
+ * Displays in CSS:
+ * #clikea-user-menu-container
+ * .clikea-user-menu-container
+ */
+function clikea_user_menu() {
+    ?>
+    <nav id="clikea-user-menu-container" class="clikea-user-menu-container">
         <?php wp_nav_menu(array('menu' => is_user_logged_in() ? 'Utilisateur connecté' : 'Utilisateur déconnecté')); ?>
     </nav>
-
     <?php
 }
 
--- inc/clikea-template-hooks.php
+++ inc/clikea-template-hooks.php
@@ -10,11 +10,13 @@ add_action('woocommerce_before_shop_loop_item_title', 'badge_new_product');
 
 add_action('clikea-navigation', 'clikea_nav_wrap_start');
 
+add_action('clikea-navigation', 'clikea_main_nav_wrap_start');
 add_action('clikea-navigation', 'clikea_main_nav_content');
 add_action('clikea-navigation', 'clikea_main_nav_wrap_end');
 
+add_action('clikea-navigation', 'clikea_shop_nav_wrap_start');
 add_action('clikea-navigation', 'clikea_shop_nav_content');
+add_action('clikea-navigation', 'clikea_shop_nav_wrap_end');
 
 add_action('clikea-navigation', 'clikea_nav_wrap_end');
 
-//add_action('clikea-cart-navigation'     ,  'clikea_cart_navigation');
