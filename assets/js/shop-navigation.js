40c0260ed19506303f0a873722f61c55bfda4b0d
charliegene14 <charliedenier@yahoo.fr>
2021-08-02 12:16:40 +0200

    shop menu nav maj

--- assets/js/shop-navigation.js
+++ assets/js/shop-navigation.js
@@ -1,14 +1,24 @@
 
-var menuItem = document.querySelectorAll('#clikea-shop-navigation-container .menu-item a');
-
+var menuItem = document.querySelectorAll ('#clikea-shop-navigation-container .menu-item a');
 
 menuItem.forEach(item => item.addEventListener('mouseover', e => {
-
     item.parentNode.parentNode.querySelectorAll('li').forEach(li => li.classList.remove('active'));
 
     //Path for Chrome OR composedPath for Firefox
     var path = e.path ? e.path : e.composedPath();
     path[1].classList.add('active');
-
-    item.parentNode.querySelector('.menu-item-has-children').classList.add('active');
+    
+    if (firstSubChild = item.parentNode.querySelector('.menu-item-has-children')) {
+        firstSubChild.classList.add('active');
+    }
 }));
+
+var subMenuWindowed = document.querySelectorAll('#clikea-shop-navigation-container .menu > .menu-item > .sub-menu');
+
+document.body.addEventListener('mousemove', e => {
+    subMenuWindowed.forEach(function(sub) {
+        if(e.clientY > sub.clientHeight + sub.getBoundingClientRect().top) {
+            sub.parentNode.classList.remove('active');
+        }
+    })
+})
